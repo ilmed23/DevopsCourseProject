b58b6c31edf0a818180958fb4884942b64c30494
EC2 Default User <ec2-user@ip-172-31-25-246.ec2.internal>
2020-01-17 18:17:41 +0000

    add load balancer

--- terraform/project_aws_infrastructure.tf
+++ terraform/project_aws_infrastructure.tf
@@ -205,10 +205,29 @@ resource "aws_launch_configuration" "Proj_LC_SwarmWorker" {
 
   # Target group for nginx
   resource "aws_lb_target_group" "Proj_HTTP_TargetGroup" {
-  name        = "tf-example-lb-tg"
+  name        = "TG_final_project"
   port        = 8080
   protocol    = "HTTP"
   vpc_id      = "${aws_vpc.project_vpc.id}"
+}
+  # Load balancer
+  resource "aws_lb" "Proj_alb" {
+  name               = "proj-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.project_security_group.id}"]
+  subnets            = ["${aws_subnet.subnet1.id}", "${aws_subnet.subnet2.id}", "${aws_subnet.subnet3.id}"]
+  }
+
+  resource "aws_lb_listener" "Proj_listener" {  
+  load_balancer_arn = "${aws_alb.Proj_alb.arn}"  
+  port              = "8080"  
+  protocol          = "HTTP"
+  
+  default_action {    
+    target_group_arn = "${aws_lb_target_group.Proj_HTTP_TargetGroup.arn}"
+    type             = "forward"
+  }
 }
 
   # AutoScaling groups - instances are created with name set to INITIALIZING, once swarm is initialized the user data will change it
